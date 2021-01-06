4237f5bd185867374c2154c5cc7b21d615dbbc09
Simon Roberts <simon@dancingcloudservices.com>
2021-01-06 11:01:20 -0700

    Day 2 Break 3

--- src/main/java/students/streams/Averager1.java
+++ src/main/java/students/streams/Averager1.java
@@ -1,6 +1,7 @@
 package students.streams;
 
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.DoubleStream;
 import java.util.stream.Stream;
 
@@ -32,10 +33,30 @@ class Average {
 
 public class Averager1 {
   public static void main(String[] args) {
-    DoubleStream.generate(() -> Math.random())
-        .limit(1000)
-        .mapToObj(v -> new Average(v, 1))
-        .reduce((a1, a2) -> a1.merge(a2))
-        .ifPresent(a -> a.get().ifPresent(v -> System.out.println("The average is " + v)));
+//    DoubleStream.generate(() -> Math.random())
+//        .limit(1000)
+//        .mapToObj(v -> new Average(v, 1))
+//        .reduce((a1, a2) -> a1.merge(a2))
+//        .ifPresent(a -> a.get().ifPresent(v -> System.out.println("The average is " + v)));
+//    DoubleStream.generate(() -> Math.random())
+//        .limit(1000)
+//        .mapToObj(v -> new Average(v, 1))
+//        .reduce(new Average(0, 0), (a1, a2) -> a1.merge(a2))
+//        .get()
+//        .ifPresent(v -> System.out.println("The average is " + v));
+    long start = System.nanoTime();
+    ThreadLocalRandom.current().doubles()
+//    DoubleStream.iterate(0.0, x -> ThreadLocalRandom.current().nextDouble())
+        .limit(1_000_000_000)
+//        .unordered()
+        .parallel()
+//        .sequential()
+//        .mapToObj(v -> v)
+        .boxed()
+        .reduce(new Average(0, 0), (a, d) -> a.include(d), (a1, a2) -> a1.merge(a2))
+        .get()
+        .ifPresent(System.out::println);
+    long time = System.nanoTime() - start;
+    System.out.println("computation took " + (time / 1_000_000_000.0) + " seconds");
   }
 }
